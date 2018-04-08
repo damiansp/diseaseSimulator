04e399948c2d66ffa956d8fadf07cb7d925d6cd6
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-04-08 16:23:55 -0400

    reorganized directory for new version

--- knitDiseaseSim.R
+++
@@ -1,5 +0,0 @@
-#install.packages('knitr', dependencies = T, repos = 'http://cran.us.r-project.org')
-library(knitr)
-
-knit2html('~/Desktop/PRA/WebsiteRoot/diseaseSim/simDeer.rmd', 
-		  '~/Desktop/PRA/WebsiteRoot/diseaseSim/simDeer' )
